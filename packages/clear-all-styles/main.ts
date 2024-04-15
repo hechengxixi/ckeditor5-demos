ce17be478588a5b21096de582600a0aa99c363f5
chengyu <hengyu@iri.cn>
2024-04-15 09:00:48 +0800

    clear all styles demo

---
+++ packages/clear-all-styles/main.ts
@@ -0,0 +1,65 @@
+// main.ts
+
+import ClassicEditor from "./src/ckeditor";
+
+class CommentsIntegration {
+  editor: ClassicEditor;
+  constructor(editor: ClassicEditor) {
+    this.editor = editor;
+  }
+
+  static get requires() {
+    return ["CommentsRepository"];
+  }
+
+  init() {
+    const usersPlugin = this.editor.plugins.get("Users");
+
+    // Load the users data.
+    const users = [
+      {
+        id: "user-1",
+        name: "Mex Haddox",
+      },
+      {
+        id: "user-2",
+        name: "Zee Croce",
+      },
+    ];
+    for (const user of users) {
+      usersPlugin.addUser(user);
+    }
+
+    // Set the current user.
+    usersPlugin.defineMe("user-1");
+  }
+}
+
+console.time("init");
+ClassicEditor
+  // Note that you do not have to specify the plugin and toolbar configuration — using defaults from the build.
+  .create(document.querySelector("#app") as HTMLElement, {
+    extraPlugins: [CommentsIntegration],
+    sidebar: {
+      container: document.getElementById("sidebar") as HTMLElement,
+    },
+    htmlSupport: {
+      allow: [
+        {
+          name: /.*/,
+          attributes: true,
+          classes: true,
+          styles: false,
+        },
+      ],
+    },
+  })
+  .then((editor) => {
+    editor.editing.view.domConverter.unsafeElements = ["script"];
+    console.timeEnd("init");
+    console.log("Editor was initialized", editor);
+
+  })
+  .catch((error) => {
+    console.error(error.stack);
+  });
