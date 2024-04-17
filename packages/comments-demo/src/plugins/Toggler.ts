1ab824292b8d6c8b4ca27ea34c4cb95a3b294dc5
chengyu <hengyu@iri.cn>
2024-04-17 18:02:30 +0800

    comments demo

---
+++ packages/comments-demo/src/plugins/Toggler.ts
@@ -0,0 +1,62 @@
+
+import { Command, Plugin } from '@ckeditor/ckeditor5-core';
+import { SwitchButtonView } from '@ckeditor/ckeditor5-ui';
+
+
+export class Toggler extends Plugin {
+    static get pluginName() {
+        return 'Toggler'
+    }
+
+    static get requires() {
+        return [ TogglerUI, TogglerCommand ]
+    }
+
+    init() {
+        const editor = this.editor;
+
+        editor.commands.add( 'toggler', new TogglerCommand( editor ) );
+
+    }
+};
+
+class TogglerUI extends Plugin {
+    init() {
+        const editor = this.editor;
+
+        editor.ui.componentFactory.add( 'toggler', locale => {
+            const view = new SwitchButtonView( locale );
+            const command = editor.commands.get( 'toggler' );
+
+            view.set( {
+                label: 'toggler',
+                withText: true,
+                tooltip: true,
+                isToggleable: true,
+                isEnabled:true,
+                isOn:true
+            } );
+
+            // A callback executed once the button is clicked.
+            view.on( 'execute', () => {
+                editor.execute( 'toggler' );
+            } );
+
+            view.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
+
+            return view;
+        } );
+    }
+};
+
+class TogglerCommand extends Command {
+    refresh() {
+        // Disable the command if the editor is in the read-only mode.
+        this.isEnabled = !this.editor.isReadOnly;
+    }
+
+    execute() {
+        // Activate pasting plain text.
+        this.value = !this.value;
+    }
+}
---
+++ packages/comments-demo/src/translations/zh-cn.ts
@@ -0,0 +1,16 @@
+import '@ckeditor/ckeditor5-alignment/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-font/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-basic-styles/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-heading/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-image/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-indent/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-link/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-list/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-table/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-html-support/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-remove-format/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-pagination/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-comments/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-track-changes/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-revision-history/build/translations/zh-cn';
+import '@ckeditor/ckeditor5-document-outline/build/translations/zh-cn';
