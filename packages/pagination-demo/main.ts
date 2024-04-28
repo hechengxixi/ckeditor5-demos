162cdc9fee5b67ff7c4f41e23563fbe712382d9e
chengyu <hengyu@iri.cn>
2024-04-28 15:34:41 +0800

    pagination demo

---
+++ packages/pagination-demo/main.ts
@@ -0,0 +1,69 @@
+// main.ts
+
+import DecoupledEditor from "./src/ckeditor";
+import html from "./src/data/demo.html?raw";
+
+class CommentsIntegration {
+  editor: DecoupledEditor;
+  constructor(editor: DecoupledEditor) {
+    this.editor = editor;
+  }
+
+  static get requires() {
+    return ["CommentsRepository"];
+  }
+
+  init() {
+    const usersPlugin = this.editor.plugins.get("Users");
+
+    // Load the users data.
+    const users = [
+      {
+        id: "user-1",
+        name: "Mex Haddox",
+      },
+      {
+        id: "user-2",
+        name: "Zee Croce",
+      },
+    ];
+    for (const user of users) {
+      usersPlugin.addUser(user);
+    }
+
+    // Set the current user.
+    usersPlugin.defineMe("user-1");
+  }
+}
+
+console.time("init");
+DecoupledEditor
+  // Note that you do not have to specify the plugin and toolbar configuration — using defaults from the build.
+  .create(document.querySelector("#app") as HTMLElement, {
+    extraPlugins: [CommentsIntegration],
+    sidebar: {
+      container: document.getElementById("sidebar") as HTMLElement,
+    },
+    htmlSupport: {
+      allow: [
+        {
+          name: /.*/,
+          attributes: true,
+          classes: true,
+          // styles: false,
+        },
+      ],
+    },
+  })
+  .then((editor) => {
+    const toolbarContainer = document.getElementById('toolbar') as HTMLElement;
+    toolbarContainer.appendChild(editor.ui.view.toolbar.element as HTMLElement);
+    console.timeEnd("init");
+    console.log("Editor was initialized", editor);
+
+    editor.setData(html)
+
+  })
+  .catch((error) => {
+    console.error(error.stack);
+  });
---
+++ packages/pagination-demo/src/ckeditor.ts
@@ -0,0 +1,11 @@
+// ckeditor.ts
+
+import { DecoupledEditor  as DecoupledEditorBase } from "@ckeditor/ckeditor5-editor-decoupled";
+import * as Plugins from "./plugins";
+import { commonConfig } from "./commonConfig";
+
+export default class DecoupledEditor extends DecoupledEditorBase {}
+
+DecoupledEditor.builtinPlugins = Object.values(Plugins);
+
+DecoupledEditor.defaultConfig = commonConfig;
